0aa7474a11e442b6c9eb0a4b7e0b1ad278ec1331
daniel <danielbaptista@smart4sistemas.com>
2021-02-03 16:07:33 -0300

    Created controllers and routes

---
+++ controllers/UserController.js
@@ -0,0 +1,8 @@
+const connection = require('connection');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+    async create(req, res) {
+        
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
 const routes = express.Router();
+const EventController = require('../controllers/EventController');
+const UserController = require('../controllers/UserController');
 
+// USER
+routes.post('/users', UserController.create);
 
+// EVENT
+routes.post('/events', EventController.create);
 
 module.exports = routes;
